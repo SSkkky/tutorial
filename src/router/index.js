903df458ec9133b51b496640e922454cfe09a779
SSkkky <worte5633@gmail.com>
2024-03-21 10:22:39 +0900

    파일 생성

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,12 @@ import HomeView from '../views/HomeView.vue'
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: '홈',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
+    name: '소개',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
